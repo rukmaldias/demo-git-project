4307728c6930302d0a57801eb71e3857e576e659
Rukmal Dias <rukmaldias@gmail.com>
2025-05-25 19:59:27 +0800

    minor change..

--- common-code/Main.java
+++ common-code/Main.java
@@ -4,9 +4,20 @@ public class Main {
 	//Line 1
         //Line 2
         //Line 3
-		// aded by thejani
-        //Line 4
-        //Line 5
+        ApplicationGraph applicationGraph = DaggerApplicationGraph.create();
+                UserRepository userRepository1 = applicationGraph.userRepository();
+                UserRepository userRepository2 = applicationGraph.userRepository();
+
+                System.out.println(userRepository1);
+                System.out.println(userRepository2);
+
+                PreferenceManager preferenceManager1 = applicationGraph.preferenceManager();
+                PreferenceManager preferenceManager2 = applicationGraph.preferenceManager();
+
+                System.out.println(preferenceManager1);
+                System.out.println(preferenceManager2);
+	
+// modified at 7.58 pm
     }
 
 }
